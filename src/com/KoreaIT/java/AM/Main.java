c3c9a76c0d79a7bd8aa944ae20c7cf8738f8ed2c
chaeyeon66 <pcycomclass66@gmail.com>
2024-02-13 11:21:09 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -7,9 +7,37 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
 
     Scanner sc = new Scanner(System.in);
-    System.out.print("명령어 ) ");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n",cmd);
+
+    int lastArticleId = 0;
+    while(true){
+      System.out.print("명령어 ) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.length() == 0){
+        System.out.println("명령어를 입력하세요");
+        continue;
+      }
+
+      if(cmd.equals("system exit")) {
+        break;
+      }
+
+      if(cmd.equals("article list")) {
+        System.out.println("게시글이 없습니다.");
+      }else if(cmd.equals("article write")){
+        lastArticleId++;
+        System.out.print("제목 : ");
+        String title = sc.nextLine();
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        System.out.printf("%d번 글이 생성 되었습니다.\n",lastArticleId);
+
+      }else{
+        System.out.println("존재하지 않는 명령어입니다.");
+      }
+    }
+
     sc.close();
 
     System.out.println("== 프로그램 종료 ==");
