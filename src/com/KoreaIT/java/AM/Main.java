903de323bbd982c7aa59b44993db9638d2431de2
chaeyeon66 <pcycomclass66@gmail.com>
2024-02-13 11:42:27 +0900

    게시글 목록 기능 구현 중

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,17 +1,20 @@
 package com.KoreaIT.java.AM;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("== 프로그램 시작 ==");
 
     Scanner sc = new Scanner(System.in);
+    List<Article> articles = new ArrayList<>();
 
     int lastArticleId = 0;
     while(true){
       System.out.print("명령어 ) ");
-      String cmd = sc.nextLine();
+      String cmd = sc.nextLine().trim();
 
       if(cmd.length() == 0){
         System.out.println("명령어를 입력하세요");
@@ -23,13 +26,21 @@ public class Main {
       }
 
       if(cmd.equals("article list")) {
-        System.out.println("게시글이 없습니다.");
+        if(articles.size() == 0) {
+          System.out.println("게시글이 없습니다.");
+        }else{
+          System.out.println("게시글이 있습니다.");
+        }
+
       }else if(cmd.equals("article write")){
         lastArticleId++;
         System.out.print("제목 : ");
         String title = sc.nextLine();
         System.out.print("내용 : ");
         String content = sc.nextLine();
+
+        Article article = new Article(lastArticleId, title, content);
+        articles.add(article);
 
         System.out.printf("%d번 글이 생성 되었습니다.\n",lastArticleId);
 
@@ -41,5 +52,17 @@ public class Main {
     sc.close();
 
     System.out.println("== 프로그램 종료 ==");
+  }
+}
+
+class Article{
+  int id;
+  String title;
+  String content;
+
+  public Article(int id, String title, String content) {
+    this.id = id;
+    this.title =title;
+    this.content = content;
   }
 }
